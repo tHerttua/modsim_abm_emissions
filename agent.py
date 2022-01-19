c312ceaa7701d871fa1ccaa0fda45bfa64605e0e
Teemu H <herttuateemu@gmail.com>
2022-01-19 18:12:28 +0200

    typo fix

--- agent.py
+++ agent.py
@@ -1,10 +1,16 @@
 
 class Agent:
-    def __init__(self, emissions_amount, allocated_credits, initial_max_price=0, intial_min_price=0):
+    def __init__(self, emissions_amount, allocated_credits, initial_max_price=0, initial_min_price=0):
         self.emissions_amount = emissions_amount
         self.allocated_credits = allocated_credits
         self.max_buying_price = initial_max_price
-        self.min_selling_price = intial_min_price
+        self.min_selling_price = initial_min_price
 
     def do_transaction(self):
+        pass
+
+    def update_selling_price(self):
+        pass
+
+    def update_buying_price(self):
         pass
