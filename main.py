3426888485b5bcc824ccd6212c49014656c9dfaa
Teemu H <herttuateemu@gmail.com>
2022-02-07 19:31:38 +0200

    Corrected allowances amount to suit 30 day month

--- main.py
+++ main.py
@@ -94,22 +94,21 @@ def chan_tran_limit(allowances, random_sel, em_red, cred_red_rate, change, limit
 200 companies split between 10 groups (every 10th percentile)
 Groups have the following emissions averages,
 and they're used as a proxy for the initial credits.
-(Subject to having divisor of 32 (days per month))
+(Subject to having divisor of 30 (days per month))
 
-monthly allowances =  = (avg_emission / 12) - (avg_emission / 12) % 32 
-NOT FINAL -> change to any other function you want
+monthly allowances = (avg_emission / 12) - (avg_emission / 12) % 30
 
 Groups  Average Emissions   Allowance credits assigned monthly
-1	    883.39              64
-2	    2886.11             160
-3   	5229.72             448
-4	    8372.33             672
-5	    14848.94            1216
-6	    22214.83            1824
-7	    32504.00            2688
-8	    49125.83            4064
-9	    89848.94            7456
-10	    366534.00           30528
+1	    883.39              60
+2	    2886.11             180
+3   	5229.72             420
+4	    8372.33             690
+5	    14848.94            1230
+6	    22214.83            1830
+7	    32504.00            2700
+8	    49125.83            4080
+9	    89848.94            7470
+10	    366534.00           30540
 
 
 
@@ -118,16 +117,16 @@ Groups  Average Emissions   Allowance credits assigned monthly
 if __name__ == '__main__':
 
     allowances = [
-        64,
-        160,
-        448,
-        672,
-        1216,
-        1824,
-        2688,
-        4064,
-        7456,
-        30528
+        60,
+        180,
+        420,
+        690,
+        1230,
+        1830,
+        2700,
+        4080,
+        7470,
+        30540
     ]
 
     #Settings
